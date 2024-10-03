cce43af7fd48e6bd833b2712ac3bdd3f148a8411
angelorc <angelo@rcstart.it>
2024-10-03 16:18:49 +0200

    chore: add social icons

--- app/app.vue
+++ app/app.vue
@@ -14,7 +14,18 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', type: 'image/png', href: '/favicon/favicon-16x16.png' },
+    { rel: 'icon', type: 'image/png', href: '/favicon/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-57x57.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-60x60.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-72x72.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-76x76.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-114x114.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-120x120.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-144x144.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-152x152.png' },
+    { rel: 'apple-touch-icon', type: 'image/png', href: '/favicon/apple-icon-180x180.png' }
   ],
   htmlAttrs: {
     lang: 'en'
@@ -23,10 +34,10 @@ useHead({
 
 useSeoMeta({
   titleTemplate: `%s - ${seo?.siteName}`,
-  ogSiteName: seo?.siteName
-  // ogImage: 'https://docs-template.nuxt.dev/social-card.png',
-  // twitterImage: 'https://docs-template.nuxt.dev/social-card.png',
-  // twitterCard: 'summary_large_image'
+  ogSiteName: seo?.siteName,
+  ogImage: 'https://delegation-dao.bitsong.io/social-card.png',
+  twitterImage: 'https://delegation-dao.bitsong.io/social-card.png',
+  twitterCard: 'summary_large_image'
 })
 
 provide('navigation', navigation)
